2b261c299c15abe262e2c4d3cd5148b68d4836e1
marlon tunes <smarlon6@gmail.com>
2025-01-25 12:50:18 -0400

    Configuracao template base

--- core/settings.py
+++ core/settings.py
@@ -76,7 +76,7 @@ THIRD_APPS = [ # são as Lib/app que instalamos no projeto
 ]
 
 PROJECT_APPS = [ # são os apps que criamos no projeto 
-        # 'apps.base',		# update 11/03/2024
+          'apps.base',
         # 'apps.myapp',   # Removido esses apps que nao criamos ainda.
 ]
 
--- core/urls.py
+++ core/urls.py
@@ -1,44 +1,11 @@
-"""
-URL configuration for core project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-import logging
-from django.http import JsonResponse
-
-# Obtenha o logger configurado
-logger = logging.getLogger('requestlogs')
-
-def test_logging_view(request):
-    try:
-        data = {'user': 'marlon', 'email': 'smarlon6@contato.com'}
-        # Força um erro proposital para testar o log
-        raise ValueError("Erro simulado no envio de email")
-    except Exception as e:
-        # Loga o erro usando o logger configurado
-        logger.error(f"{str(e)} | {str(data)}")
-        return JsonResponse({"status": "error", "message": "Ocorreu um erro."})
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test-logging/', test_logging_view, name='test_logging'),
 ]
 
 if settings.DEBUG: # update 03/11/2024: (em homologa com debug true adiciona rota static)
